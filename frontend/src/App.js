bc86801ca7145c068ddfb24abf38dbaae54119e2
Filip <fkobus.coding@gmail.com>
2025-03-08 15:48:11 +0100

    Initalized react project

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Tldraw } from 'tldraw'
+import 'tldraw/tldraw.css'
+
+export default function App() {
+	return (
+		<div style={{ position: 'fixed', inset: 0 }}>
+			<Tldraw />
+		</div>
+	)
+}
+
