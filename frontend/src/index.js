ec73cd16741bf142d2b2276ca1332d5d4c458cba
Filip <fkobus.coding@gmail.com>
2025-03-30 22:32:03 +0200

    Moved config.js to .env

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import config from './config';
 import { Amplify } from 'aws-amplify'
 
 
 Amplify.configure({
     Auth: {
-      Cognito: {
-        userPoolClientId: config.cognito.APP_CLIENT_ID,
-        userPoolId: config.cognito.USER_POOL_ID,
+      Cognito: { 
+        userPoolClientId: process.env.REACT_APP_COGNITO_CLIENT_ID,
+        userPoolId: process.env.REACT_APP_COGNITO_USER_POOL_ID,
         loginWith: {
             email: 'true',
           }
